19599b571389b0c61961cff889243d1fb129b5ca
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-28 14:08:37 +0200

    feat: :sparkles: add the LeftSide component

---
+++ src/modules/projects/components/layouts/LeftSide.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-navigation-drawer>
+    <v-list>
+      <v-list-item title="PROJECTS" subtitle="There are no projects yet"></v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-list-item link title="List Item 1"></v-list-item>
+    <v-list-item link title="List Item 2"></v-list-item>
+    <v-list-item link title="List Item 3"></v-list-item>
+  </v-navigation-drawer>
+</template>
