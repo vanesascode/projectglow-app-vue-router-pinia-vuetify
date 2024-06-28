f21fef2abe6def053e191f50f04e6372697424d0
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-28 14:08:15 +0200

    refactor: :recycle: modify the FloatingButton component styles

--- src/modules/common/components/FloatingButton.vue
+++ src/modules/common/components/FloatingButton.vue
@@ -1,17 +1,20 @@
 <template>
-  <v-fab icon="mdi-plus" @click="$emit('click')"></v-fab>
+  <v-fab icon="mdi-plus" @click="$emit('click')" class="floating-button"></v-fab>
 </template>
 
 <script setup lang="ts">
 defineEmits(['click']);
 </script>
 
-<style>
-.v-btn {
+<style lang="scss">
+.floating-button {
   margin: 0;
   position: fixed;
   bottom: 50px;
-  right: 50px;
-  background-color: rgb(143, 43, 158);
+  right: 80px;
+
+  button {
+    background-color: rgb(143, 43, 158);
+  }
 }
 </style>
