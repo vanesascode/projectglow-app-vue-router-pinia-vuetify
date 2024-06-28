d50969a38fedf7fa1b64f9259073b1ec371fad9b
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-28 08:07:13 +0200

    feat: :sparkles: add the floating button common component

---
+++ src/modules/common/components/FloatingButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-fab icon="mdi-plus" @click="$emit('click')"></v-fab>
+</template>
+
+<script setup lang="ts">
+defineEmits(['click']);
+</script>
+
+<style>
+.v-btn {
+  margin: 0;
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  background-color: rgb(143, 43, 158);
+}
+</style>
