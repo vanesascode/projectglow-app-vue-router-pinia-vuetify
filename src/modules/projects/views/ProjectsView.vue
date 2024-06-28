123bcfe87f7fa17e831e0249d40f9eb18926b931
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-28 14:08:54 +0200

    feat: :sparkles: add the ProjectsView component

---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left"></th>
+        <th class="text-left">Project</th>
+        <th class="text-left">Task</th>
+        <th class="text-left">Progress</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>My project</td>
+        <td>My task 1</td>
+        <td>50%</td>
+      </tr>
+    </tbody>
+  </v-table>
+  <InputModal />
+</template>
+
+<script lang="ts">
+import InputModal from '@/modules/projects/components/ProjectsView/InputModal.vue'
+
+export default {
+  components: {
+    InputModal
+  }
+}
+</script>
