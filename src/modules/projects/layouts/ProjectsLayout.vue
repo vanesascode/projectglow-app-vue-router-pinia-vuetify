1ede2b598acc21c6f3003efffb19adb8bd20dfd5
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-28 14:09:29 +0200

    feat: :sparkles: add the ProjectsLayout component

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app class="rounded rounded-md">
+    <v-app-bar title="Notion"></v-app-bar>
+
+    <LeftSide />
+    <v-main class="" style="min-height: 300px">
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import LeftSide from '@/modules/projects/components/layouts/LeftSide.vue'
+</script>
