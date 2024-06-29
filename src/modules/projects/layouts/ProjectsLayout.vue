5780bea68aa339cad7cd334ca55b408831907111
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 22:38:32 +0200

    feat: :sparkles: add a route and style to the navbar title in the projectsLayout component

--- src/modules/projects/layouts/ProjectsLayout.vue
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app class="rounded rounded-md">
-    <v-app-bar title="Notion"></v-app-bar>
+    <RouterLink to="/#/projects" class="text-decoration-none">
+      <v-app-bar title="PROJECTS MANAGEMENT" class="project-title"></v-app-bar>
+    </RouterLink>
 
     <LeftSide />
     <v-main class="" style="min-height: 300px">
@@ -10,5 +12,17 @@
 </template>
 
 <script setup lang="ts">
-import LeftSide from '@/modules/projects/components/layouts/LeftSide.vue'
+import LeftSide from '@/modules/projects/components/layouts/LeftSide.vue';
 </script>
+
+<style lang="scss">
+@import '@/assets/styles/main.scss';
+
+.project-title {
+  color: white;
+  font-size: 1.15rem;
+  &:hover {
+    color: $primary-color;
+  }
+}
+</style>
