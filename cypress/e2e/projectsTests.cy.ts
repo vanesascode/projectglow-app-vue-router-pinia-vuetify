810b01e93b497c03ca6640c3f033cc04b29bdc51
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 21:23:33 +0200

    refactor: :recycle: change the urls in the projects cypress tests

--- cypress/e2e/projectsTests.cy.ts
+++ cypress/e2e/projectsTests.cy.ts
@@ -1,28 +1,28 @@
 // https://on.cypress.io/api
 describe('projects tests', () => {
   it('create a project clicking on the save button', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('.mdi-plus').click();
-    cy.get('#input-8').clear();
-    cy.get('#input-8').type('New Project');
+    cy.get('#input').clear();
+    cy.get('#input').type('New Project');
     cy.get('[type="submit"] > .v-btn__content').click();
     cy.get('tbody > :nth-child(1) > :nth-child(2)').should('exist').contains('Project');
   });
 
   it('enter a project page', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
   });
 
   it('create a project pressing the intro key', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('.mdi-plus').click();
-    cy.get('#input-8').clear();
-    cy.get('#input-8').type('New Project{enter}');
+    cy.get('#input').clear();
+    cy.get('#input').type('New Project{enter}');
   });
 
   it('go back to projects after entering a project page', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
     cy.get('.v-breadcrumbs-item--link').click();
   });
