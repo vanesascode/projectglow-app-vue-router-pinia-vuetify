74507ab147205e59fc398517bfe3ef127e0c3a1f
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 19:48:34 +0200

    feat: :sparkles: add the BreadCrumbs component

---
+++ src/modules/common/components/BreadCrumbs.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-breadcrumbs :items="items"></v-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+interface Props {
+  name: string;
+}
+
+const props = defineProps<Props>();
+
+interface BreadcrumbItem {
+  title: string;
+  disabled: boolean;
+  href: string;
+}
+
+const items = ref<BreadcrumbItem[]>([]);
+
+watch(
+  () => props.name,
+  () => {
+    items.value = [
+      {
+        title: 'Projects',
+        disabled: false,
+        href: '/projects',
+      },
+      {
+        title: props.name,
+        disabled: true,
+        href: '',
+      },
+    ];
+  },
+  {
+    immediate: true,
+  },
+);
+</script>
