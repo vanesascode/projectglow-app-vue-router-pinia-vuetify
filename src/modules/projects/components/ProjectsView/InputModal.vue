b5de062a0fa57c0771a513fcae5a5610a00f92b9
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 19:52:10 +0200

    refactor: :recycle: modify the InputModal component to make it reusable

--- src/modules/projects/components/ProjectsView/InputModal.vue
+++ src/modules/projects/components/ProjectsView/InputModal.vue
@@ -1,30 +1,45 @@
 <template>
   <v-dialog max-width="500" v-model="isModalOpen">
-    <v-card title="New Project">
+    <v-card :title="title">
       <v-form @submit.prevent>
         <v-text-field
           v-model="inputValue"
-          label="Add a name to your new project"
+          :label="label"
           ref="inputRef"
+          @keydown.enter="submitValue"
         ></v-text-field>
       </v-form>
 
       <v-card-actions>
         <v-spacer></v-spacer>
         <div>
-          <v-btn type="submit" @click="submitValue">Submit</v-btn>
-          <v-btn text="Close Dialog" @click="closeModal"></v-btn>
+          <v-btn type="submit" @click="submitValue">Save</v-btn>
+          <v-btn text="Close" @click="closeModal"></v-btn>
         </div>
       </v-card-actions>
     </v-card>
   </v-dialog>
 
-  <FloatingButton @click="isModalOpen = true" />
+  <FloatingButton
+    @click="
+      isModalOpen = true;
+      focusInput();
+    "
+    icon="mdi-plus"
+    color="rgb(143, 43, 158)"
+  />
 </template>
 
 <script setup lang="ts">
 import FloatingButton from '@/modules/common/components/FloatingButton.vue';
-import { ref } from 'vue';
+import { onUpdated, ref } from 'vue';
+
+interface Props {
+  title: string;
+  label: string;
+}
+
+defineProps<Props>();
 
 const emits = defineEmits<{
   value: [text: string];
@@ -33,6 +48,16 @@ const emits = defineEmits<{
 const inputValue = ref('');
 const isModalOpen = ref(false);
 const inputRef = ref<HTMLInputElement | null>(null);
+
+const focusInput = () => {
+  inputRef.value?.focus();
+};
+
+onUpdated(() => {
+  if (isModalOpen.value === true) {
+    inputRef.value?.focus();
+  }
+});
 
 const submitValue = () => {
   if (!inputValue.value) {
