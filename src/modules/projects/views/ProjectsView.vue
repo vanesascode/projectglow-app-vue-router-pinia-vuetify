86914747c1134a775b618745ded27cdd769dc494
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 19:51:37 +0200

    refactor: :recycle: modify the ProjectsView page to fit the created projects info

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -4,28 +4,52 @@
       <tr>
         <th class="text-left"></th>
         <th class="text-left">Project</th>
-        <th class="text-left">Task</th>
+        <th class="text-left">Tasks</th>
         <th class="text-left">Progress</th>
       </tr>
     </thead>
+
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>My project</td>
-        <td>My task 1</td>
-        <td>50%</td>
+      <tr
+        class="projects-row"
+        v-for="(project, index) in projectsStore.projectsWithCompletition"
+        :key="project.id"
+        @click="$router.push(`/project/${project.id}`)"
+      >
+        <td>{{ index + 1 }}</td>
+        <td>{{ project.name }}</td>
+        <td>{{ project.tasks.length }}</td>
+        <td>
+          <v-progress-linear :model-value="project.progress" height="25" color="rgb(143, 43, 158)"
+            ><strong>
+              {{ project.progress ? project.progress + '%' : 'No tasks yet' }}</strong
+            ></v-progress-linear
+          >
+        </td>
       </tr>
     </tbody>
   </v-table>
-  <InputModal />
+  <InputModal
+    @value="projectsStore.addProject"
+    title="New Project"
+    label="Add a name to your project"
+  />
 </template>
 
-<script lang="ts">
-import InputModal from '@/modules/projects/components/ProjectsView/InputModal.vue'
+<script setup lang="ts">
+import InputModal from '@/modules/projects/components/ProjectsView/InputModal.vue';
+import { useProjectsStore } from '@/modules/projects/store/projects.store';
 
-export default {
-  components: {
-    InputModal
+const projectsStore = useProjectsStore();
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/main.scss';
+
+.projects-row {
+  cursor: pointer;
+  &:hover {
+    color: $primary-color;
   }
 }
-</script>
+</style>
