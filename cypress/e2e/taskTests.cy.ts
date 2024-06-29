920c96e6e4dc51b9c89c5ddb31ef64eb62c95d20
vanesa <vanesa.juarez.paris@gmail.com>
2024-06-29 21:23:58 +0200

    refactor: :recycle: change the urls and ids in the task cypress tests

--- cypress/e2e/taskTests.cy.ts
+++ cypress/e2e/taskTests.cy.ts
@@ -2,24 +2,24 @@
 
 describe('tasks tests', () => {
   it('create a task clicking on the save button', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
     cy.get('.mdi-plus').click();
-    cy.get('#input-9').clear();
-    cy.get('#input-9').type('New Task');
+    cy.get('#input').clear();
+    cy.get('#input').type('New Task');
     cy.get('[type="submit"] > .v-btn__content').click();
   });
 
   it('create a task pressing the intro key', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
     cy.get('.mdi-plus').click();
-    cy.get('#input-9').clear();
-    cy.get('#input-9').type('New task{enter}');
+    cy.get('#input').clear();
+    cy.get('#input').type('New task{enter}');
   });
 
   it('click on the checkbox to mark it as completed', () => {
-    cy.visit('/');
+    cy.visit('https://projects-management-nine.vercel.app');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').click();
     cy.get(':nth-child(1) > :nth-child(1) > .checkbox').check();
   });
