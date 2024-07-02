0a55a3f3ab15af9d46a84b55a9e1c6ce198cfa33
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-02 16:29:20 +0200

    feat: :sparkles: add the index file to export all the plugins

---
+++ src/plugins/index.ts
@@ -0,0 +1,18 @@
+// import { loadFonts } from './webfontloader'
+// import vuetify from './vuetify';
+// import vueI18n from './i18n'
+import router from '@/router';
+import { createPinia } from 'pinia';
+import { toast, options } from './toastification';
+
+// Types
+import type { App } from 'vue';
+
+export function registerPlugins(app: App) {
+  //   loadFonts()
+  app.use(createPinia());
+  app.use(router);
+  //   app.use(vueI18n)
+  app.use(toast, options);
+  // app.use(vuetify);
+}
