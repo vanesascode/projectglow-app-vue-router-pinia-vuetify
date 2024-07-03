1c3b5ae4155227b271f5b347d23e01576366e9ea
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-03 15:10:38 +0200

    feat: :sparkles: add the ProjectView component

--- src/views/projects/ProjectView.vue
+++ src/views/projects/ProjectView.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
+import { useProjectsStore } from '@/stores/projects.store';
+import type { Project } from '@/modules/projects/interfaces/project.interface';
+import { ref, watch } from 'vue';
+import InputModal from '@/modules/projects/components/ProjectsView/InputModal.vue';
+
+import { useRouter } from 'vue-router';
+
+interface Props {
+  id: string;
+}
+
+const router = useRouter();
+const props = defineProps<Props>();
+const projectsStore = useProjectsStore();
+const project = ref<Project>();
+
+watch(
+  () => props.id,
+  () => {
+    project.value = projectsStore.projectList.find((project) => project.id === props.id);
+    if (project.value === undefined) {
+      router.replace('/projects');
+    }
+  },
+  {
+    immediate: true,
+  },
+);
+</script>
+
 <template>
   <section>
     <BreadCrumbs :name="project?.name ?? 'Project does not exist'" />
@@ -33,38 +65,6 @@
     label="Add a name to your task"
   />
 </template>
-
-<script setup lang="ts">
-import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
-import { useProjectsStore } from '@/modules/projects/store/projects.store';
-import type { Project } from '@/modules/projects/interfaces/project.interface';
-import { ref, watch } from 'vue';
-import InputModal from '@/modules/projects/components/ProjectsView/InputModal.vue';
-
-import { useRouter } from 'vue-router';
-
-interface Props {
-  id: string;
-}
-
-const router = useRouter();
-const props = defineProps<Props>();
-const projectsStore = useProjectsStore();
-const project = ref<Project>();
-
-watch(
-  () => props.id,
-  () => {
-    project.value = projectsStore.projectList.find((project) => project.id === props.id);
-    if (project.value === undefined) {
-      router.replace('/projects');
-    }
-  },
-  {
-    immediate: true,
-  },
-);
-</script>
 
 <style lang="scss">
 @import '@/assets/styles/main.scss';
