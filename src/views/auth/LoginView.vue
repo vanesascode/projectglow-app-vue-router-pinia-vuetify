30bf3bbb3df1e677509021995819493d680d8c76
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-03 15:15:54 +0200

    feat: :sparkles: add the login view component

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Login View</h1>
+  <div class="d-flex ga-6">
+    <RouterLink to="/welcome">LOGIN</RouterLink>
+
+    <RouterLink to="/register">or register here</RouterLink>
+  </div>
+</template>
