dceba16b1fce0a48d27517965549c389a297a1be
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-03 16:13:28 +0200

    feat: :sparkles: add the clients interfaces

---
+++ types/api/client.d.ts
@@ -0,0 +1,15 @@
+export interface Client {
+  id: number;
+  name: string;
+  logoUrl: string | null;
+  isEnabled: boolean;
+  description: string | null;
+  projects: Project[];
+}
+
+export interface ClientState {
+  clients: Client[];
+  client: Client;
+  total: number;
+  loading: boolean;
+}
