2c3c06a5cdbc89be73550d5a3ac1ac6464cf04ca
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-03 14:59:09 +0200

    feat: :sparkles: add the vue-toastification template

---
+++ src/components/modals/ToastTemplate.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+export interface Props {
+  title: string;
+  detail: Record<string, Array<string>>;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  detail: () => {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <div>
+    <b>{{ props.title }}</b>
+    <p v-for="(items, field) in props.detail" :key="'toast-field-' + field">
+      {{ field }}:
+      <br />
+      <span v-for="(item, index) in items" :key="'toast-item-' + index"> - {{ item }}. </span>
+      <br />
+    </p>
+  </div>
+</template>
