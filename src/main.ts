61fa013067a8f7e78037fa20df8afba86fb37620
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-03 14:58:43 +0200

    refactor: :recycle: add the vuetify plugin to the main.ts file throught the registerPlugins index.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+// import { createVuetify } from 'vuetify';
+// import * as components from 'vuetify/components';
+// import * as directives from 'vuetify/directives';
 import { registerPlugins } from '@/plugins';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'dark',
-  },
-});
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+//   theme: {
+//     defaultTheme: 'dark',
+//   },
+// });
 
 const app = createApp(App);
 
-app.use(vuetify);
+// app.use(vuetify);
 
 registerPlugins(app);
 
