529b9fd80383879269a3731081f86e13f53e3572
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-04 23:00:47 +0200

    refactor: :recycle: fix the project types

--- types/api/project.d.ts
+++ types/api/project.d.ts
@@ -1,7 +1,18 @@
 import { Task } from './task';
 
 export interface Project {
-  id: string;
+  id: number;
   name: string;
+  description?: string | null;
+  isEnabled: boolean;
   tasks: Task[];
+  progress: number;
+}
+
+export interface ProjectState {
+  // projectsWithCompletition: any[];
+  projects: Project[];
+  project: Project;
+  total: number;
+  loading: boolean;
 }
