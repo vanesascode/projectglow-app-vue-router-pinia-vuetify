084ccacca1364b51af1bd1454434af18b7e65b00
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-04 15:17:46 +0200

    refactor: :recycle: fix the emit that sends the data of the form in the InputModal component

--- src/components/main/InputModal.vue
+++ src/components/main/InputModal.vue
@@ -1,52 +1,17 @@
-<template>
-  <v-dialog max-width="500" v-model="isModalOpen">
-    <v-card :title="title">
-      <v-form @submit.prevent>
-        <v-text-field
-          id="input"
-          v-model="inputValue"
-          :label="label"
-          ref="inputRef"
-          @keydown.enter="submitValue"
-        ></v-text-field>
-      </v-form>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <div>
-          <v-btn type="submit" @click="submitValue">Save</v-btn>
-          <v-btn text="Close" @click="closeModal"></v-btn>
-        </div>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-
-  <FloatingButton
-    @click="
-      isModalOpen = true;
-      focusInput();
-    "
-    icon="mdi-plus"
-    color="rgb(143, 43, 158)"
-  />
-</template>
-
 <script setup lang="ts">
 import FloatingButton from '@/components/main/FloatingButton.vue';
 import { onUpdated, ref } from 'vue';
 
 interface Props {
   title: string;
-  label: string;
+  name: string;
+  description: string;
 }
 
 defineProps<Props>();
 
-const emits = defineEmits<{
-  value: [text: string];
-}>();
-
-const inputValue = ref('');
+const nameValue = ref('');
+const descriptionValue = ref('');
 const isModalOpen = ref(false);
 const inputRef = ref<HTMLInputElement | null>(null);
 
@@ -60,20 +25,64 @@ onUpdated(() => {
   }
 });
 
+const emits = defineEmits<{
+  newClient: [{ name: string; description: string }];
+}>();
+
 const submitValue = () => {
-  if (!inputValue.value) {
+  // console.log(nameValue.value, descriptionValue.value);
+  if (!nameValue.value) {
     inputRef.value?.focus();
     return;
   }
 
-  emits('value', inputValue.value.trim());
+  emits('newClient', { name: nameValue.value.trim(), description: descriptionValue.value.trim() });
 
-  inputValue.value = '';
+  nameValue.value = '';
+  descriptionValue.value = '';
   isModalOpen.value = false;
 };
 
 const closeModal = () => {
-  inputValue.value = '';
+  nameValue.value = '';
+  descriptionValue.value = '';
   isModalOpen.value = false;
 };
 </script>
+
+<template>
+  <v-dialog max-width="500" v-model="isModalOpen">
+    <v-card :title="title">
+      <v-form @submit.prevent>
+        <v-text-field
+          id="nameInput"
+          v-model="nameValue"
+          :label="name"
+          ref="inputRef"
+        ></v-text-field>
+        <v-text-field
+          id="DescriptionInput"
+          v-model="descriptionValue"
+          :label="description"
+        ></v-text-field>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <div>
+            <v-btn type="submit" @click="submitValue">Save</v-btn>
+            <v-btn text="Close" @click="closeModal"></v-btn>
+          </div>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-dialog>
+
+  <FloatingButton
+    @click="
+      isModalOpen = true;
+      focusInput();
+    "
+    icon="mdi-plus"
+    color="#299145"
+  />
+</template>
