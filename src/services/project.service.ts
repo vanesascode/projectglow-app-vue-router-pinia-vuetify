561cb0c62d1db657245e566572e68d8d5a55f8af
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-05 09:54:28 +0200

    feat: :sparkles: add the addProject method in the project service

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -8,6 +8,13 @@ class ProjectService {
     const response = await httpClient.get(`${baseUrl}/${clientId}/projects`);
     return response.data;
   };
+  addProject = async (
+    newProject: { name: string; description: string; isEnabled: boolean },
+    clientId: number,
+  ): Promise<Project> => {
+    const response = await httpClient.post(`${baseUrl}/${clientId}/projects`, newProject);
+    return response.data;
+  };
 }
 
 export default new ProjectService();
