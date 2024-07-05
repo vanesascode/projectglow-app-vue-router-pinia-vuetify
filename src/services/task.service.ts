241740efc5d5ea2f87ff3966e6725d27294e6b63
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-05 10:47:00 +0200

    feat: :sparkles: add the deleteTask method in the task service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -5,7 +5,7 @@ const baseUrl = '/clients';
 
 class TaskService {
   getTasks = async (clientId: number, projectId: number): Promise<Task[]> => {
-    const response = await httpClient.get(`${baseUrl}/${clientId}/projects/${projectId}`);
+    const response = await httpClient.get(`${baseUrl}/${clientId}/projects/${projectId}/tasks`);
     return response.data;
   };
 
@@ -17,6 +17,13 @@ class TaskService {
     const response = await httpClient.post(
       `${baseUrl}/${clientId}/projects/${projectId}/tasks`,
       task,
+    );
+    return response.data;
+  };
+
+  deleteTask = async (task: Task, clientId: number, projectId: number): Promise<Task> => {
+    const response = await httpClient.delete(
+      `${baseUrl}/${clientId}/projects/${projectId}/tasks/${task.id}`,
     );
     return response.data;
   };
