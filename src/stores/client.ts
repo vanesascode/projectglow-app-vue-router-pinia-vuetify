0ff24824b6237f50c89a69653331d85cb2102bad
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-05 09:55:51 +0200

    refactor: :recycle: modify the addNewClient store method name so it is different from the service addClient method

--- src/stores/client.ts
+++ src/stores/client.ts
@@ -33,7 +33,7 @@ export const useClientsStore = defineStore({
       }
     },
 
-    async addClient({
+    async addNewClient({
       name: nameValue,
       description: descriptionValue,
     }: {
