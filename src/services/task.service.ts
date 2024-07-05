adbc4fd2ab931709b7e2cd410b1fa2a95c6699d8
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-05 09:54:48 +0200

    feat: :sparkles: add the addTask method in the task service file

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -8,6 +8,18 @@ class TaskService {
     const response = await httpClient.get(`${baseUrl}/${clientId}/projects/${projectId}`);
     return response.data;
   };
+
+  addTask = async (
+    task: { name: string; description: string },
+    clientId: number,
+    projectId: number,
+  ): Promise<Task> => {
+    const response = await httpClient.post(
+      `${baseUrl}/${clientId}/projects/${projectId}/tasks`,
+      task,
+    );
+    return response.data;
+  };
 }
 
 export default new TaskService();
