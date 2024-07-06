64ccc77be4061bbcb145d3094b47dada36187fcc
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-06 10:53:57 +0200

    refactor: :recycle: fix the tasks route in the router index file

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,13 @@ import MainAuthView from '@/views/MainAuthView.vue';
 import MainProjectsView from '@/views/MainProjectsView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import clientRoutes from './clientRoutes';
-import projectsRoutes from './projecstRoutes';
 import authRoutes from './authRoutes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // AUTH VIEW
+
     {
       path: '/',
       name: 'auth',
@@ -16,11 +17,15 @@ const router = createRouter({
       component: MainAuthView,
       children: [...authRoutes],
     },
+
+    // WECOME VIEW
     {
       path: '/welcome',
       name: 'home',
       component: () => import('@/views/auth/WelcomeView.vue'),
     },
+
+    // CLIENTS VIEW
 
     {
       path: '/clients',
@@ -28,13 +33,32 @@ const router = createRouter({
       component: MainClientsView,
       children: [...clientRoutes],
     },
+
+    // PROJECTS VIEW
+
     {
-      path: '/clients/:clientId/projects',
+      path: '/clients/:clientId/:clientName/projects',
       name: 'ClientProjects',
       component: MainProjectsView,
-      children: [...projectsRoutes],
+      children: [
+        {
+          path: '',
+          name: 'projects',
+          props: true,
+          component: () => import('@/views/projects/ProjectsView.vue'),
+        },
+      ],
     },
 
+    // TASKS VIEW
+    {
+      path: '/clients/:clientId/:clientName/projects/:projectId/:projectName',
+      props: true,
+      name: 'Tasks',
+      component: () => import('@/views/projects/ProjectView.vue'),
+    },
+
+    // 404
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
