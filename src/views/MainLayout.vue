0880314aa06074ce0a384b07ac94da83bf87157e
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-07 14:06:59 +0200

    feat: :sparkles: add a logout icon in the MainLayout view

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,8 +1,13 @@
 <template>
   <v-app class="rounded rounded-md">
-    <v-app-bar title="PROJECTGLOW">
+    <v-app-bar title="PROJECTGLOW" scroll-behavior="hide">
       <template v-slot:prepend>
         <img src="https://i.postimg.cc/d3Q5mhbJ/logo.png" alt="" height="40rem" class="ms-2" />
+      </template>
+      <template v-slot:append>
+        <RouterLink to="/login">
+          <v-btn icon="mdi-logout" color="white"></v-btn>
+        </RouterLink>
       </template>
     </v-app-bar>
 
