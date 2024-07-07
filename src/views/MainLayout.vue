0f63bfd8ee68e9df4e47f508d86810db82638b3c
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-07 13:19:12 +0200

    refactor: :recycle: remove link from logo in the MainLayout file

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,12 +1,11 @@
 <template>
   <v-app class="rounded rounded-md">
-    <RouterLink to="/#/projects" class="text-decoration-none">
-      <v-app-bar title="CLIENTS MANAGEMENT" class="project-title">
-        <template v-slot:prepend>
-          <img src="https://i.postimg.cc/d3Q5mhbJ/logo.png" alt="" height="40rem" class="ms-2" />
-        </template>
-      </v-app-bar>
-    </RouterLink>
+    <v-app-bar title="PROJECTGLOW">
+      <template v-slot:prepend>
+        <img src="https://i.postimg.cc/d3Q5mhbJ/logo.png" alt="" height="40rem" class="ms-2" />
+      </template>
+    </v-app-bar>
+
     <v-main class="" style="min-height: 300px">
       <RouterView />
     </v-main>
