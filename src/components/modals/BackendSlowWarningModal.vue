bc7b57975fb4aeecf2c8bccec9495a90bac25fd9
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-07 18:19:29 +0200

    feat: :sparkles: add the BackendSlowWarning modal

---
+++ src/components/modals/BackendSlowWarningModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import FloatingButton from '@/components/main/FloatingButton.vue';
+import { onMounted, ref } from 'vue';
+
+const isModalOpen = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    isModalOpen.value = true;
+  }, 1000);
+});
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+</script>
+
+<template>
+  <v-dialog max-width="500" v-model="isModalOpen">
+    <v-card :title="title">
+      <v-card-text class="text-subtitle-1">
+        The backend is slow to respond. It's located on a free onRender server.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <div>
+          <v-btn text="Ok" @click="closeModal"></v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style lang="scss">
+.v-card-text::before {
+  content: 'Please be patient: ';
+  color: rgb(183, 62, 62);
+  font-weight: bold;
+}
+</style>
