49c9766f5e2df258bf929e84d93645fbe9fcc405
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-07 10:22:53 +0200

    refactor: :recycle: remove obsolete vuetify configuration and set to dark in general

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,15 +6,10 @@ import * as directives from 'vuetify/directives';
 // Composables
 import { createVuetify } from 'vuetify';
 
-import config from '../../configuration.json';
-
 export default createVuetify({
   components,
   directives,
   theme: {
-    defaultTheme: config.defaultTheme,
-    themes: {
-      ...config.themes,
-    },
+    defaultTheme: 'dark',
   },
 });
