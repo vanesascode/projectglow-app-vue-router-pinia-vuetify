d88e6dff3e83c80682c7d71abacd0ccd353fc019
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-08 20:09:09 +0200

    feat: :sparkles: add internationalization to the vuetify components

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,14 +2,25 @@ import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { en, es } from 'vuetify/locale'
 
 // Composables
 import { createVuetify } from 'vuetify';
+
+const storedLanguage = localStorage.getItem('language') || 'es-ES';
 
 export default createVuetify({
   components,
   directives,
   theme: {
     defaultTheme: 'dark',
+  },
+  locale: {
+    locale: storedLanguage,
+    fallback: 'es-ES',
+    messages: {
+      'es-ES': es,
+      'en-EN': en
+    },
   },
 });
