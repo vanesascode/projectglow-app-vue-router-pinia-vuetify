d7da8923cc86f97e394c25b65675d1b0f482229f
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-08 20:21:49 +0200

    refactor: :recycle: remove unnecessary vue-i18n import in the LoginView component

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import { useI18n } from 'vue-i18n'
 
-
-const { t } = useI18n()
 const router = useRouter();
 const visible = ref(false);
 const inputRef = ref<HTMLInputElement | null>(null);
