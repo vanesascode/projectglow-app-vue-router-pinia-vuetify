4b48a99444f2f15abbbe0af62f393a88b1094625
Vanesa Juarez <vanesa.juarez.paris@gmail.com>
2024-07-08 16:26:59 +0200

    refactor: :recycle: delete the register route in the authRoutes file for not using it in the end

--- src/router/authRoutes.ts
+++ src/router/authRoutes.ts
@@ -6,11 +6,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Login',
     component: () => import('@/views/auth/LoginView.vue'),
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('@/views/auth/RegisterView.vue'),
-  },
 ];
 
 export default routes;
